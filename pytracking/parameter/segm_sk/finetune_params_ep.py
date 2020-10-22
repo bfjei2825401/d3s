e407eda76091ef5307809ecf901859b1a249495a
bfjei2825401 <752958525@qq.com>
2020-10-22 22:24:05 +0800

    add sk_finetune batch testing

--- pytracking/parameter/segm_sk/finetune_params_ep.py
+++ pytracking/parameter/segm_sk/finetune_params_ep.py
@@ -3,5 +3,5 @@ from pytracking.parameter.segm import finetune_params
 
 def parameters(ep_num):
     params = finetune_params.parameters()
-    params.segm_net_path = '/home/slz/GitHub/d3s/save/checkpoints/ltr/segm_sk/segm_sk/SegmNet_ep00{:0>2d}.pth.tar'.format(ep_num)
+    params.segm_net_path = '/home/slz/GitHub/d3s/save/checkpoints/ltr/segm_sk/segm_sk_finetune/SegmNet_ep00{:0>2d}.pth.tar'.format(ep_num)
     return params
