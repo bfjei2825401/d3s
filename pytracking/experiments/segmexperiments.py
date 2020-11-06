039edbdcff2dc4fd6a2788f7f03c35c883dc41dc
bfjei2825401 <752958525@qq.com>
2020-11-06 17:13:01 +0800

    add otb and uav123 experiment for segm tracker

--- pytracking/experiments/segmexperiments.py
+++ pytracking/experiments/segmexperiments.py
@@ -2,24 +2,7 @@ from pytracking.evaluation import Tracker, OTBDataset, NFSDataset, UAVDataset, T
     TrackingNetDataset, LaSOTDataset, GOT10KDatasetTest
 
 
-def atom_nfs_uav():
-    # Run three runs of ATOM on NFS and UAV datasets
-    trackers = [Tracker('atom', 'default', i) for i in range(3)]
-
-    dataset = NFSDataset() + UAVDataset()
-    return trackers, dataset
-
-
-def uav_test():
-    # Run ATOM and ECO on the UAV dataset
-    trackers = [Tracker('atom', 'default', i) for i in range(1)] + \
-               [Tracker('eco', 'default', i) for i in range(1)]
-
-    dataset = UAVDataset()
-    return trackers, dataset
-
-
 def segm_test():
     trackers = [Tracker('segm', 'default_params', i) for i in range(1)]
-    dataset = LaSOTDataset() + TrackingNetDataset() + GOT10KDatasetTest()
+    dataset = OTBDataset() + UAVDataset() + LaSOTDataset() + TrackingNetDataset() + GOT10KDatasetTest()
     return trackers, dataset
